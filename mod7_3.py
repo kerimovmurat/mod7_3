20c90dac9b816a8b7db85674fb6dc4723c363c1f
kerimovmurat <kerimovmurat@yandex.ru>
2024-09-17 22:44:07 +0300

    Add files via upload

---
+++ mod7_3.py
@@ -0,0 +1,39 @@
+import string
+
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='cp1251') as file:
+                content = file.read().lower()
+                for punct in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                    content = content.replace(punct, '')
+                words = content.split()
+                all_words[file_name] = words
+        return all_words
+
+    def find(self, word):
+        word = word.lower()
+        all_words = self.get_all_words()
+        result = {}
+        for file_name, words in all_words.items():
+            if word in words:
+                result[file_name] = words.index(word) + 1
+        return result
+
+    def count(self, word):
+        word = word.lower()
+        all_words = self.get_all_words()
+        result = {}
+        for file_name, words in all_words.items():
+            result[file_name] = words.count(word)
+        return result
+
+# Пример использования класса
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))     # 3 слово по счёту
+print(finder2.count('teXT'))    # 4 слова teXT в тексте всего
