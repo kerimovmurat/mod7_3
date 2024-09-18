f1ed5897787b67be1e4ee1ef899b0593b5b6aad5
kerimovmurat <kerimovmurat@yandex.ru>
2024-09-18 18:50:47 +0300

    Update mod7_3.py

--- mod7_3.py
+++ mod7_3.py
@@ -6,11 +6,11 @@ class WordsFinder:
         all_words = {}
         for file_name in self.file_names:
             with open(file_name, 'r', encoding='cp1251') as file:
-                content = file.read().lower()
-                for punct in [',', '.', '=', '!', '?', ';', ':', ' - ']:
-                    content = content.replace(punct, '')
-                words = content.split()
-                all_words[file_name] = words
+                content = file.read().lower() # cодержимое файла читается и преобразуется в нижний регистр для унификации
+                for punct in [',', '.', '=', '!', '?', ';', ':', ' - ']: # цикл проходит по списку знаков препинания
+                    content = content.replace(punct, '') #   метод replace заменяет все вхождения строки punct на пустую строку ''
+                words = content.split() # метода split() # cодержимое файла разбивается на отдельные слова
+                all_words[file_name] = words # cписок слов добавляется в словарь all_words с именем файла в качестве ключа
         return all_words
 
     def find(self, word):
